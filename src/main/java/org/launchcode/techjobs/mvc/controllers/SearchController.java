668c9ef7b0b4381e638cc784cdf8b4119a69aae0
Andy <Afisch2014@gmail.com>
2023-03-01 23:05:40 -0800

    man, there are 7 failing tests here, but it behaves just as the demo app does.

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.Job;
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -15,9 +21,25 @@ import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoic
 @RequestMapping("search")
 public class SearchController {
 
+    ArrayList<Job> jobs = new ArrayList<>();
+
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
+        return "search";
+    }
+
+    @PostMapping(value ="results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        model.addAttribute("search", jobs);
+        model.addAttribute("columns", columnChoices);
+        if (searchTerm.equals("") || (searchTerm.equals("all"))) {
+            jobs = JobData.findAll();
+            model.addAttribute("all", JobData.findAll());
+        }else jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+        model.addAttribute("jobs", jobs);
+
         return "search";
     }
 
